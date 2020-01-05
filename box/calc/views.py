6b0d4bf05f0d7266d525d1f9fd92ae34446cabc5
ankitnathtiwari <ankitfounder@gmail.com>
2020-01-05 18:18:46 +0530

    Views update

--- box/calc/views.py
+++ box/calc/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello world")
+    return HttpResponse("Hi Bro how are you, It will take some more time")
 
