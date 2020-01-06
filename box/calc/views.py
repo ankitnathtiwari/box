1d5471d06af0bbea1c646792717aeab357a65f96
ankitnathtiwari <ankitfounder@gmail.com>
2020-01-06 15:47:45 +0530

    views check 1

--- box/calc/views.py
+++ box/calc/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hi Bro how are you, It will take some more time, we are almost there")
+    return HttpResponse("Hi Bro how are you, It will take some more time, we are almost there, Its working I think")
 
